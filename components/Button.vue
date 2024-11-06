8cec243febb4cd231c1acb5f2fd508e9a9bf95e8
nutka <tuba4080@outlook.com>
2024-11-06 20:44:48 +0100

    index.vue,Time.vue,Button.vue作成

---
+++ components/Button.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="button">
+        <div class="content-message">{{ message }}</div>
+        <div class="first-button" v-if="!work.work_start && !work.work_end">
+            <button class="work-start" @click="workStart">勤務開始</button>
+        </div>
+        <div class="second-button" v-else>
+            <button class="break-start" v-if="!rest.break_start && !rest.break_end" @click="breakStart">休憩開始</button>
+            <button class="break-end" v-else @click="breakEnd">休憩終了</button>
+            <button class="work-end" @click="workEnd">勤務終了</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: null,
+        }
+    },
+    props: {
+        work: {
+            type: Object,
+            required: true,
+        },
+        rest: {
+            type: Object,
+            required: true,
+        },
+    },
+    methods: {
+        async workStart() {
+            try {
+                const { data } = await this.$axios.post('http://localhost/api/auth/work_start', {
+                    user_id: this.$auth.user.id,
+                });
+                this.message = data.message;
+                this.$emit('update-work', data.work);
+            } catch (error) {
+                if (error.response && error.response.data && error.response.data.error) {
+                    alert(`エラーが発生しました: ${error.response.data.error}`);
+                } else {
+                    alert('予期せぬエラーが発生しました');
+                }
+            }
+        },
+        async breakStart() {
+            try {
+                const { data } = await this.$axios.post('http://localhost/api/auth/break_start', {
+                    user_id: this.$auth.user.id,
+                });
+                this.message = data.message;
+                this.$emit('update-break', data.rest);
+            } catch (error) {
+                if (error.response && error.response.data && error.response.data.error) {
+                    alert(`エラーが発生しました: ${error.response.data.error}`);
+                } else {
+                    alert('予期せぬエラーが発生しました');
+                }
+            }
+        },
+        async breakEnd() {
+            try {
+                const { data } = await this.$axios.post('http://localhost/api/auth/break_end', {
+                    user_id: this.$auth.user.id,
+                });
+                this.message = data.message;
+                this.$emit('update-break', data.rest);
+            } catch (error) {
+                if (error.response && error.response.data && error.response.data.error) {
+                    alert(`エラーが発生しました: ${error.response.data.error}`);
+                } else {
+                    alert('予期せぬエラーが発生しました');
+                }
+            }
+        },
+        async workEnd() {
+            try {
+                const { data } = await this.$axios.post('http://localhost/api/auth/work_end', {
+                    user_id: this.$auth.user.id,
+                });
+                this.message = data.message;
+                this.$emit('update-work', data.work);
+            } catch (error) {
+                if (error.response && error.response.data && error.response.data.error) {
+                    alert(`エラーが発生しました: ${error.response.data.error}`);
+                } else {
+                    alert('予期せぬエラーが発生しました');
+                }
+            }
+        },
+    },
+};
+</script>
---
+++ components/Time.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="time">{{ currentTime }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentTime: new Date().toLocaleTimeString(),
+            timer: null,
+        };
+    },
+    methods: {
+        updateCurrentTime() {
+            this.currentTime = new Date().toLocaleTimeString();
+        },
+    },
+    mounted() {
+        this.timer = setInterval(this.updateCurrentTime, 1000);
+    },
+    beforeDestroy() {
+        if (this.timer) {
+            clearInterval(this.timer);
+        }
+    },
+};
+</script>
