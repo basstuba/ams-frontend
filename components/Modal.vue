cd12e30019730a2c7d23703f577b74b3757b8d50
nutka <tuba4080@outlook.com>
2024-11-11 10:21:55 +0100

    休憩詳細のモーダルウィンドウ作成

---
+++ components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="modal">
+        <button class="modal-button__close" @click="$emit('close')">閉じる</button>
+        <table class="modal-table">
+            <tr class="modal-data__title">
+                <th class="title-name">休憩開始</th>
+                <th class="title-name">休憩終了</th>
+                <th class="title-name">休憩時間</th>
+            </tr>
+            <tr class="modal-data__item" v-for="rest in modalRestsData" :key="rest.id">
+                <td class="item-detail">{{ rest.break_start }}</td>
+                <td class="item-detail">{{ rest.break_end }}</td>
+                <td class="item-detail">{{ rest.break_time }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        modalRestsData: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
--- pages/daily.vue
+++ pages/daily.vue
@@ -6,11 +6,11 @@
         </div>
         <div class="search">
             <div class="search-date__before">
-                <img class="before-icon" @click="dayBefore" src="../assets/images/left-triangle.png" alt="前日">
+                <img class="before-icon" @click="getDayBefore" src="../assets/images/left-triangle.png" alt="前日">
             </div>
-            <div class="search-date">{{ formattedDate }}</div>
+            <div class="search-date">{{ formattedDay }}</div>
             <div class="search-date__after">
-                <img class="after-icon" @click="dayAfter" src="../assets/images/right-triangle.png" alt="翌日">
+                <img class="after-icon" @click="getDayAfter" src="../assets/images/right-triangle.png" alt="翌日">
             </div>
         </div>
         <table class="daily-table">
@@ -28,11 +28,12 @@
                 <td class="item-detail">{{ work.work_end }}</td>
                 <td class="item-detail">{{ work.break_total }}</td>
                 <td class="item-detail">
-                    <Modal :modalRestsData="work.rests" />
+                    <button class="modal-open" @click="openModal(work.rests)">詳細</button>
                 </td>
                 <td class="item-detail">{{ work.work_time }}</td>
             </tr>
         </table>
+        <Modal v-if="isModalOpen" :modalRestsData="selectedRests" @close="isModalOpen = false" />
     </div>
 </template>
 
@@ -41,18 +42,15 @@ export default {
     layout: 'another',
     data() {
         return {
+            formattedDay: null,
             onTheDay: null,
             dayBefore: null,
             dayAfter: null,
             works: [],
             filteredWorks: [],
+            isModalOpen: false,
+            selectedRests: [],
         };
-    },
-    computed: {
-        formattedDate() {
-            const [year, month, day] = this.onTheDay.split('-');
-            return `${parseInt(month)}月${parseInt(day)}日`;
-        },
     },
     methods: {
         async getDailyData() {
@@ -63,6 +61,7 @@ export default {
                 this.dayAfter = data.day_after;
                 this.works = data.works;
                 this.getFilteredWorksData();
+                this.formattedDate();
             } catch (error) {
                 if (error.response && error.response.data && error.response.data.error) {
                     alert(`勤怠一覧の取得に失敗しました: ${error.response.data.error}`);
@@ -71,16 +70,18 @@ export default {
                 }
             }
         },
-        async dayBefore() {
+        async getDayBefore() {
             try {
                 const { data } = await this.$axios.post('http://localhost/api/auth/day_before', {
-                    day_before: this.day_before,
+                    day_before: this.dayBefore,
                 });
                 this.onTheDay = data.on_the_day;
                 this.dayBefore = data.day_before;
                 this.dayAfter = data.day_after;
                 this.works = data.works;
+                this.isModalOpen = false;
                 this.getFilteredWorksData();
+                this.formattedDate();
             } catch (error) {
                 if (error.response && error.response.data && error.response.data.error) {
                     alert(`勤怠一覧の取得に失敗しました: ${error.response.data.error}`);
@@ -89,16 +90,18 @@ export default {
                 }
             }
         },
-        async dayAfter() {
+        async getDayAfter() {
             try {
                 const { data } = await this.$axios.post('http://localhost/api/auth/day_after', {
-                    day_after: this.day_after,
+                    day_after: this.dayAfter,
                 });
                 this.onTheDay = data.on_the_day;
                 this.dayBefore = data.day_before;
                 this.dayAfter = data.day_after;
                 this.works = data.works;
+                this.isModalOpen = false;
                 this.getFilteredWorksData();
+                this.formattedDate();
             } catch (error) {
                 if (error.response && error.response.data && error.response.data.error) {
                     alert(`勤怠一覧の取得に失敗しました: ${error.response.data.error}`);
@@ -109,13 +112,21 @@ export default {
         },
         getFilteredWorksData() {
             const filteredData = this.works.map(work => ({
-                name: work.user.name,
-                workStart: work.work_start,
-                workEnd: work.work_end,
-                breakTotal: work.break_total,
-                workTime: work.work_time
+                氏名: work.user.name,
+                勤務開始: work.work_start,
+                勤務終了: work.work_end,
+                休憩時間: work.break_total,
+                勤務時間: work.work_time
             }));
             this.filteredWorks = filteredData;
+        },
+        formattedDate() {
+            const [year, month, day] = this.onTheDay.split('-');
+            this.formattedDay = `${parseInt(month)}月${parseInt(day)}日`;
+        },
+        openModal(rests) {
+            this.selectedRests = rests;
+            this.isModalOpen = true;
         },
     },
     created() {
